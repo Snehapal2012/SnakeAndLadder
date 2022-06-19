a6b63f35c461e73d114be07ba52cc33cb8b95453
snehapal2012 <snehapal2012@gmail.com>
2022-06-19 09:31:37 +0530

    Added: Welcome to Snake and Ladder Simulator Program

---
+++ src/com/snakeandladder/SnakeAndLadderMain.java
@@ -0,0 +1,7 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Simulator");
+    }
+}
