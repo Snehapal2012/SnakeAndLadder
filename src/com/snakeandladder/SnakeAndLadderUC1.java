085c8dd295a6ff34b82dc56de6b05bcdda69fc6d
snehapal2012 <snehapal2012@gmail.com>
2022-06-19 09:36:41 +0530

    Modified: Initial position of a player

---
+++ src/com/snakeandladder/SnakeAndLadderUC1.java
@@ -0,0 +1,11 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderUC1 {
+
+        public static void main(String[] args) {
+            int pos=0;
+            System.out.println("The initial position of the player: "+pos);
+
+
+        }
+}
